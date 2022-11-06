e4fb0108430ab45c4d7e2ffa4c14fed0d001eadc
Mircea-George Cozarev <mircea2501@gmail.com>
2022-11-06 19:11:57 +0200

    Small modifications

--- src/components/HotelListItem.tsx
+++ src/components/HotelListItem.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { HotelProps } from './useHotels'
 import moment from 'moment/moment'
+import { IonItem } from '@ionic/react'
 
 const HotelListItem: React.FC<HotelProps> = (hotel) => {
   return (
-    <div>
-      <p>{hotel.name}</p>
-      <p>{`Active since: ${moment(hotel.dateRegistered).toString()}`}</p>
-      <p>Capacity: {hotel.capacity}</p>
-      <p>{hotel.isAvailable ? 'Available' : 'Unavailable'}</p>
-    </div>
+    <IonItem>
+      <div>
+        <p>{hotel.name}</p>
+        <p>{`Active since: ${moment(hotel.dateRegistered).toString()}`}</p>
+        <p>Capacity: {hotel.capacity}</p>
+        <p>{hotel.isAvailable ? 'Available' : 'Unavailable'}</p>
+      </div>
+    </IonItem>
   )
 }
 
