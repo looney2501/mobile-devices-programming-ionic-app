f6f2740ea2dc5038b3be5145c2119e23e4cb907a
Mircea-George Cozarev <mircea2501@gmail.com>
2022-11-25 12:13:57 +0200

    Add Hotel details modal with animation

---
+++ src/components/hotel/HotelDetailsModal.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { HotelProps } from '../../reducers/reducer'
+import { createAnimation, IonButton, IonModal } from '@ionic/react'
+import moment from 'moment'
+
+interface HotelDetailsModalProps {
+  hotelProps: HotelProps,
+  showModal: boolean,
+  setShowModal: Function
+}
+
+const HotelDetailsModal: React.FC<HotelDetailsModalProps> = ({ hotelProps, showModal, setShowModal }) => {
+  const enterAnimation = (baseEl: any) => {
+    const root = baseEl.shadowRoot;
+
+    const backdropAnimation = createAnimation()
+      .addElement(root.querySelector('ion-backdrop')!)
+      .fromTo('opacity', '0', '1');
+
+    const wrapperAnimation = createAnimation()
+      .addElement(root.querySelector('.modal-wrapper')!)
+      .keyframes([
+        { offset: 0, opacity: '0', transform: 'scale(0)' },
+        { offset: 1, opacity: '0.99', transform: 'scale(1)' }
+      ]);
+
+    return createAnimation()
+      .addElement(baseEl)
+      .easing('ease-out')
+      .duration(500)
+      .addAnimation([backdropAnimation, wrapperAnimation]);
+  }
+
+  const leaveAnimation = (baseEl: any) => {
+    return enterAnimation(baseEl).direction('reverse');
+  }
+
+
+  return (
+    <IonModal isOpen={showModal} enterAnimation={enterAnimation} leaveAnimation={leaveAnimation}>
+      <div style={{ height: "100%", display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <div style={{ margin: "0 10px" }}>
+          {!hotelProps._id && (
+            <p style={{ backgroundColor: "yellow", color: "black" }}>!!!Locally saved only</p>
+          )}
+          <p>{hotelProps.name}</p>
+          <p>{`Active since: ${moment(hotelProps.dateRegistered).toString()}`}</p>
+          <p>Capacity: {hotelProps.capacity}</p>
+          <p>{hotelProps.isAvailable ? 'Available' : 'Unavailable'}</p>
+        </div>
+        <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+      </div>
+    </IonModal>
+  )
+}
+
+export default HotelDetailsModal
--- src/components/hotel/HotelListItem.tsx
+++ src/components/hotel/HotelListItem.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment/moment'
-import { IonItem } from '@ionic/react'
+import { IonButton, IonItem } from '@ionic/react'
 import { HotelProps } from '../../reducers/reducer'
+import HotelDetailsModal from './HotelDetailsModal'
 
 const HotelListItem: React.FC<HotelProps> = (hotel) => {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <IonItem>
-      <div>
-        {!hotel._id && (
-          <p style={{ backgroundColor: "yellow", color: "black" }}>!!!Locally saved only</p>
-        )}
-        <p>{hotel.name}</p>
-        <p>{`Active since: ${moment(hotel.dateRegistered).toString()}`}</p>
-        <p>Capacity: {hotel.capacity}</p>
-        <p>{hotel.isAvailable ? 'Available' : 'Unavailable'}</p>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+        <div>
+          {!hotel._id && (
+            <p style={{ backgroundColor: "yellow", color: "black" }}>!!!Locally saved only</p>
+          )}
+          <p>{hotel.name}</p>
+          <p>{`Active since: ${moment(hotel.dateRegistered).toString()}`}</p>
+          <p>Capacity: {hotel.capacity}</p>
+          <p>{hotel.isAvailable ? 'Available' : 'Unavailable'}</p>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <IonButton onClick={() => setShowModal(true)}>Show Details</IonButton>
+        </div>
       </div>
+      <HotelDetailsModal hotelProps={hotel} showModal={showModal} setShowModal={setShowModal} />
     </IonItem>
   )
 }
